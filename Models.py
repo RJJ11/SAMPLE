be4d704e55038ead005f5d421faa940673cc3e12
RJJ11 <rohitjjoseph@gmail.com>
2015-08-31 16:20:05 +0530

    Removed another stray 'head' command.

--- Models.py
+++ Models.py
@@ -71,27 +71,6 @@ class ProfileMiniForm(messages.Message):
     clubsJoined = messages.StringField(9,repeated=True)
 
 
-
-<<<<<<< HEAD
-class Club(ndb.Model):
-	name = ndb.StringProperty(required=True)
-	clubId = ndb.StringProperty(required=True)
-	admin = ndb.StringProperty(required=True)
-	description = ndb.StringProperty(required=True)
-	members = ndb.StringProperty(repeated=True)
-	followers = ndb.StringProperty(repeated=True) # Only includes the set of people apart from members. By default a member of a club follows it.
-	abbreviation = ndb.StringProperty()
-	photo = ndb.BlobProperty()
-	isAlumni = ndb.StringProperty(required=True)
-	collegeId = ndb.StringProperty(required=True)
-	id = clubId
-
-class ClubMiniForm(messages.Message):
-    """ClubMiniForm -- What's shown on the UI for a club"""
-    name = messages.StringField(1,required=True)
-    abbreviation = messages.StringField(2,required=True)
-    '''photo =''' 
-||||||| merged common ancestors
 class Club(ndb.Model):
 	name = ndb.StringProperty(required=True)
 	clubId = ndb.StringProperty(required=True)
