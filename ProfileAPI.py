4b480648da00111657edbef3c6e70a8db8f02fb3
rishabkdoshi <rishabketandoshi@gmail.com>
2015-10-18 00:50:25 +0530

    Add code for getProfileFromEmail

--- ProfileAPI.py
+++ ProfileAPI.py
@@ -20,34 +20,28 @@ def _copyProfileToForm(prof):
         pf.check_initialized()
         return pf
 
-def _getProfileFromUser():
-        """Return user Profile from datastore"""
-        user = endpoints.get_current_user()
-        if not user:
-            raise endpoints.UnauthorizedException('Authorization required fuck you')
 
-        user_id=Profile.query(Profile.email==user.email()).fetch(1)
+def _getProfileFromEmail(email):
+        user_id=Profile.query(Profile.email==email).fetch(1)
         if len(user_id)>0:
+            print "User Existed"            
             return user_id[0]
-            #p_key = ndb.Key(Profile, user_id[0])
-            #profile = p_key.get()
-
+            
         else:
+            print "New User"
             return Profile(name = '',
                            email = '',
                            phone = '',
                            isAlumni='N',
                            )
-        #else:
-         #   return user_id[0]
+    
 
-
-def _doProfile(save_request=None):
+def _doProfile(email,save_request=None):
         """Get user Profile and return to user, possibly updating it first."""
-        prof = _getProfileFromUser()
-        pf = ProfileMiniForm()
-
+        prof = _getProfileFromEmail(email)
+        
         if save_request:
+            pf = ProfileMiniForm()
             for field in pf.all_fields():
                 collegeLocation=getattr(save_request,'collegeLocation')
                 print collegeLocation,"is location"
