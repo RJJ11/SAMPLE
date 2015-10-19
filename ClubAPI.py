afc98563de0b2c380b3dab94b44b758731ad21b7
MAC <macbook@MACs-MBP.Home>
2015-10-20 00:15:35 +0400

    Created a create club api, approve club,dealt with from and to pid

--- ClubAPI.py
+++ ClubAPI.py
@@ -26,7 +26,7 @@ def createClub(request=None):
 
         if request and college :
 
-            for field in ('abbreviation','club_name','from_pid','to_pid','isAlumni','collegeId','description'):
+            for field in ('abbreviation','club_name','from_pid','to_pid','isAlumni','collegeId','description','approval_status'):
 
 
                 if field == "abbreviation":
@@ -37,37 +37,75 @@ def createClub(request=None):
                     clubRequest.description = request.description
 
                 elif field == "from_pid":
-                     print("Entered from_pid")
+                     '''print("Entered from_pid")
                      profile =  Profile(
                             name = 'SiddharthSend',
                             email = 'sid.tiger184@gmail.com',
                             phone = '7760531993',
                             isAlumni='N',
                             collegeId=college_key
-                            )
-                     profile_key = profile.put()
+                            )'''
+                     profile_key = ndb.Key('Profile',int(request.from_pid))
                      print("Finished frompid")
                      setattr(clubRequest, field, profile_key)
                 elif field == "to_pid":
-                     print("Entered To PID")
+                     '''print("Entered To PID")
                      profile =  Profile(
                                name = 'SiddharthRec',
                                email = 'sid.tiger183@gmail.com',
                                phone = '7760531994',
-
                                isAlumni='N',
                                collegeId=college_key
-                               )
-                     profile_key = profile.put()
+                               )'''
+                     #get the college of the from_pid guy
+                     #Get the email of the college
+                     #correspond it to the SUP profile
+                     #get his key and save it
 
-                     setattr(clubRequest, field, profile_key)
+                     from_pid_key = ndb.Key('Profile',int(request.from_pid))
+                     from_profile = from_pid_key.get()
+
+                     print("From Profile is",from_profile)
+                     college_key = from_profile.collegeId
+                     college = college_key.get()
+
+                     email = college.sup_emailId
+
+                     print("Email is",email)
+
+                     query = Profile.query(Profile.email == email).fetch(1)
+
+                     if(query[0]):
+                         to_pid_key = query[0].key
+                         print("to_pid_key",to_pid_key)
+                         setattr(clubRequest, field, to_pid_key)
                 elif field == "isAlumni":
                      setattr(clubRequest, field, "N")
                 elif field == "collegeId":
                      setattr(clubRequest, field, college_key)
+                elif field == "approval_status":
+                     setattr(clubRequest, field, "N")
             clubRequest.put()
 
         return clubRequest
+def approveClub(requestentity = None):
+    ''' In this, first check who the request's to pid is for. If its for SUP of college then change approval status to Y
+     else keeep it as it is and flag error'''
+
+    if(requestentity):
+        sup_profile = requestentity.to_pid.get()
+        college = requestentity.collegeId.get()
+
+        if(sup_profile.email == college.sup_emailId):
+            requestentity.approval_status = "Y"
+            return "Y"
+        else :
+            requestentity.approval_status ="N"
+            return "N"
+    print("Modified Request Entity is",requestentity)
+    requestentity.put()
+
+
 
 
 def createClubAfterApproval(requestentity=None):
@@ -81,6 +119,7 @@ def createClubAfterApproval(requestentity=None):
             newClub.isAlumni = requestentity.isAlumni
             newClub.description = requestentity.description
             newClub.members.append(requestentity.from_pid)
+            newClub.follows.append(requestentity.from_pid)
             clubkey = newClub.put()
 
             profile = requestentity.from_pid.get()
@@ -132,4 +171,44 @@ def getClub(request=None):
              retClub.collegeName = college[0].name
              retClub.description = club.description
         return retClub
+
+def unfollowClub(request):
+        ''' steps that need to be done
+        Get the profile and the club
+        If the profile matches that of the club admin then disallow
+
+        Check if the profile is a follower of a club. If true remove from each other's lists
+
+
+        '''
+        print("Request for unfollow is ",request)
+
+        from_pid = ndb.Key('Profile',int(request.from_pid))
+        club_id = ndb.Key('Club',int(request.club_id))
+
+
+        profile = from_pid.get()
+        club = club_id.get()
+
+        print("Profile",profile)
+        print("Club",profile)
+
+        if(club.admin == from_pid):
+            return False
+
+        if(from_pid in club.follows):
+
+         #remove club id from profile followers list
+         #remove profile id from clubs followers list
+         club.follows.remove(from_pid)
+         profile.follows.remove(club_id)
+
+         club.put()
+         profile.put()
+         return True
+
+        else:
+         return False
+
+
 
