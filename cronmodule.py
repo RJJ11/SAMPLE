7c3ebed877cc0f003a4fbaca2f556e5634073852
SidPee <sid.tiger183@gmail.com>
2016-01-20 11:19:28 +0530

    Added cronmodule and pc_photo.py

---
+++ cronmodule.py
@@ -0,0 +1,9 @@
+from EventsAPI import getEventsBasedonTimeLeft
+import logging
+import datetime
+
+
+logging.basicConfig(level=logging.DEBUG)
+LOG = logging.getLogger(__name__)
+LOG.info("Entered the Cron")
+getEventsBasedonTimeLeft()#fill with current time later
